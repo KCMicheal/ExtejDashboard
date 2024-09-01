ef6a87440b4b713d50686d19a5ba0a3d09e6f38d
KCM <egwunwokekc@gmail.com>
2024-09-01 13:08:20 +0100

    Tailwind install and Logo input

---
+++ ExtejDashboard/tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{razor,html,cshtml}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#3A57E8',
+        'secondary': '#5227D3',
+        'primary-bg': '#F5F5F5',
+        'secondary-bg': '#FDF5E6',
+        'dark': '#263238',
+        'accent': '#D63230',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+      },
+      fontWeight: {
+        'light': 300,
+        'normal': 400,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 700,
+        'extrabold': 800,
+        'black': 900,
+      },
+    },
+  },
+  plugins: [],
+}
+
