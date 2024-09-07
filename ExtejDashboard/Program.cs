0e125392ae24dbdfc81107bbd7d4d8e47988f072
KCM <egwunwokekc@gmail.com>
2024-09-07 02:44:33 +0100

    main: applied cors and changed base ref

--- ExtejDashboard/Program.cs
+++ ExtejDashboard/Program.cs
@@ -2,19 +2,31 @@ using ExtejDashboard.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+string? origins = "origins";
 // Add services to the container.
 builder.Services.AddRazorComponents();
-builder.Services.AddCors();
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(origins,
+						  policy =>
+						  {
+							  policy.AllowAnyOrigin()
+												  .AllowAnyHeader()
+												  .AllowAnyMethod();
+						  });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
+
+app.UseCors(origins);
 
 app.UseHttpsRedirection();
 
